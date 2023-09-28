a921239bce8d30d500be4bdab3c5092a5732ffd3
fernandom11399 <jafetavalos49@gmail.com>
2023-09-28 08:35:28 -0600

    Initial commit

---
+++ app/src/main/java/com/example/aplicacion_para_cambiar_de_pestana/Aplicacion_De_La_Siguiente_Pestana.java
@@ -0,0 +1,15 @@
+package com.example.aplicacion_para_cambiar_de_pestana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Aplicacion_De_La_Siguiente_Pestana extends AppCompatActivity {
+
+    Button ex1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aplicacion_de_la_siguiente_pestana);
+    }
+}
